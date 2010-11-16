d21b21c480daeb8e1a32b650e224f2245077a4f4
Gregor Martynus <gregor@martynus.net>
2010-11-16 23:53:54 +0100

    cleaning up

--- index.js
+++ index.js
@@ -10,70 +10,64 @@
 
 require.paths.unshift('./vendor');
 
-var creds = {
-  account:  'yolk',
-  api_key:  '76315504aa8df50'
-};
+var creds = { account:    'yolk'
+            , api_key:    '76315504aa8df50'
+            , user_agent: 'mite.node example/v0.1'},
+    MiteResources = require(__dirname + '/lib/resources'),
+    Mite = new MiteResources(creds);
 
 function dump(data) {
   console.log(data);
 }
 
-var UserResource = require(__dirname + '/lib/resources/user'),
-    User         = new UserResource(creds);
-// User.all(dump);
-// User.archived(dump);
-// User.find(1, dump);
-// User.time_entries(1, dump)
-
-var CustomerResource = require(__dirname + '/lib/resources/customer'),
-    Customer         = new CustomerResource(creds);
-// Customer.all(dump);
-// Customer.archived(dump);
-// Customer.new({name: 'funky uno!'}, dump);
-// Customer.find(1, dump);
-// Customer.update(1, {name: 'funky zwuno!'}, dump);
-// Customer.delete(16, dump);
-// Customer.time_entries(1, dump)
-// Customer.projects(1, dump)
-
-var ProjectResource = require(__dirname + '/lib/resources/project'),
-    Project         = new ProjectResource(creds);
-// Project.all(dump);
-// Project.archived(dump);
-// Project.new({name: 'funky uno!'}, dump);
-// Project.find(1, dump);
-// Project.update(1, {name: 'funky zwuno!'}, dump);
-// Project.delete(20, dump);
-// Project.time_entries(1, dump)
-
-var ServiceResource = require(__dirname + '/lib/resources/service'),
-    Service         = new ServiceResource(creds);
-// Service.all(dump);
-// Service.archived(dump);
-// Service.new({name: 'funky uno!'}, dump);
-// Service.find(1, dump);
-// Service.update(1, {name: 'funky zwuno!'}, dump);
-// Service.delete(24, dump);
-// Service.time_entries(1, dump)
-
-var TimeEntryResource = require(__dirname + '/lib/resources/time_entry'),
-    TimeEntry         = new TimeEntryResource(creds);
-// TimeEntry.all(dump);
-// TimeEntry.archived(dump);
-// TimeEntry.new({name: 'funky uno!'}, dump);
-// TimeEntry.find(3024, dump);
-// TimeEntry.update(3024, {note: 'funky zwuno!'}, dump);
-// TimeEntry.delete(3024, dump);
-
-var TrackerResource = require(__dirname + '/lib/resources/tracker'),
-    Tracker         = new TrackerResource(creds);
-// Tracker.find(dump);
-// Tracker.start(3064, dump);
-// Tracker.stop(3064,dump);
-
-var BookmarkResource = require(__dirname + '/lib/resources/bookmark'),
-    Bookmark         = new BookmarkResource(creds);
-// Bookmark.all(dump);
-// Bookmark.find(6, dump);
-// Bookmark.time_entries(6, dump);
+// // User
+// Mite.User.all(dump);
+// Mite.User.archived(dump);
+// Mite.User.find(1, dump);
+// Mite.User.time_entries(1, dump);
+// 
+// // Customer
+// Mite.Customer.all(dump);
+// Mite.Customer.archived(dump);
+// Mite.Customer.create({name: 'funky uno!'}, dump);
+// Mite.Customer.find(1, dump);
+// Mite.Customer.update(1, {name: 'funky zwuno!'}, dump);
+// Mite.Customer.delete(16, dump);
+// Mite.Customer.time_entries(1, dump)
+// Mite.Customer.projects(1, dump)
+// 
+// // Project
+// Mite.Project.all(dump);
+// Mite.Project.archived(dump);
+// Mite.Project.create({name: 'funky test'}, dump);
+// Mite.Project.find(24, dump);
+// Mite.Project.update(24, {name: 'funky fresh!'}, dump);
+// Mite.Project.delete(20, dump);
+// Mite.Project.time_entries(1, dump)
+// 
+// // Service
+// Mite.Service.all(dump);
+// Mite.Service.archived(dump);
+// Mite.Service.create({name: 'funky uno!'}, dump);
+// Mite.Service.find(1, dump);
+// Mite.Service.update(1, {name: 'funky zwuno!'}, dump);
+// Mite.Service.delete(24, dump);
+// Mite.Service.time_entries(1, dump)
+// 
+// // TimeEntry
+// Mite.TimeEntry.all(dump);
+// Mite.TimeEntry.archived(dump);
+// Mite.TimeEntry.create({name: 'funky uno!'}, dump);
+// Mite.TimeEntry.find(3024, dump);
+// Mite.TimeEntry.update(3024, {note: 'funky zwuno!'}, dump);
+// Mite.TimeEntry.delete(3024, dump);
+// 
+// // Tracker
+// Mite.Tracker.find(dump);
+// Mite.Tracker.start(3064, dump);
+// Mite.Tracker.stop(3064,dump);
+// 
+// // Bookmark
+// Mite.Bookmark.all(dump);
+// Mite.Bookmark.find(6, dump);
+// Mite.Bookmark.time_entries(6, dump);
